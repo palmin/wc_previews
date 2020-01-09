a3f82fdf1c510b2103fc01fb54e7c27222a146d9
Anders Borum <anders@algoritmer.dk>
2020-01-09 14:52:54 +0100

    add markdown preview mode

---
+++ markdown_Custom.js
@@ -0,0 +1,17 @@
+
+function renderMarkdown(markdown, element) {
+  element.innerHTML = htmlFromMarkdown(markdown);
+  
+  document.querySelectorAll('pre code').forEach((block) => {
+    hljs.highlightBlock(block);
+  });
+}
+
+function renderPreview(url) {
+  fetch(url, { method: 'get' })
+    .then(response => response.text())
+    .then(text => renderMarkdown(text, document.getElementById("content")))
+    .catch(err => {
+  	console.error(err);	
+  })
+}
